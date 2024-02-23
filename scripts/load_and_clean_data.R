695ea6de446e4dd4a9d2d99e05ff0b48673b313b
Daniel Sussman <sussman@bu.edu>
2024-02-23 11:08:31 -0500

    Initial commit

---
+++ scripts/load_and_clean_data.R
@@ -0,0 +1,13 @@
+# This file is purely as an example. 
+# There are a few places
+
+library(tidyverse)
+
+loan_data <- read_csv(here::here("dataset", "loan_refusal.csv"))
+
+## CLEAN the data
+loan_data_clean <- loan_data
+
+write_csv(loan_data_clean, file = here::here("dataset", "loan_refusal_clean.csv"))
+
+save(loan_data_clean, file = here::here("dataset/loan_refusal.RData"))
